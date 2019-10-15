0314681f7f98d61149054fe268a09b3eba4a00d7
我演示 <1456191267@qq.com>
2019-10-15 16:52:47 +0800

    提交

---
+++ DEMO/DEMO/NotionControl.h
@@ -0,0 +1,12 @@
+//
+//  NotionControl.h
+//  DEMO
+//
+//  Created by 我演示 on 2019/10/15.
+//  Copyright © 2019 Jerry-Fu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const XMGCategoryNotioncation ;
+
---
+++ DEMO/DEMO/NotionControl.m
@@ -0,0 +1,11 @@
+//
+//  NotionControl.m
+//  DEMO
+//
+//  Created by 我演示 on 2019/10/15.
+//  Copyright © 2019 Jerry-Fu. All rights reserved.
+//
+
+#import "NotionControl.h"
+
+NSString *const  XMGCategoryNotioncation = @"XMGCategoryNotioncation";
